45f49d46991db62e0608e146cc610cf6e61014c1
xiehuixin <73511929+xiehuixin@users.noreply.github.com>
2021-02-06 21:15:35 +0800

    Delete test_demo.py

--- test_requests/test_demo.py
+++
@@ -1,8 +0,0 @@
-# 导入requests库
-import requests
-
-
-def test_demo():
-    r = requests.request(method="GET", url="https://httpbin.testing-studio.com/get")
-    print(r.json())
-    print(r.status_code)
